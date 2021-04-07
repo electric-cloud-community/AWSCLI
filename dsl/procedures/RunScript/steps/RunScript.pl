13265a836924d6823460461e075505e353a4ff2f
Polina Shubina <925043@gmail.com>
2021-04-07 20:11:56 +0300

    Moving away from bintray

---
+++ dsl/procedures/RunScript/steps/RunScript.pl
@@ -0,0 +1,6 @@
+$[/myProject/perl/core/scripts/preamble.pl]
+
+use FlowPlugin::AWSCLI;
+# Auto generated code of plugin step
+# Go to dsl/properties/perl/lib/EC/Plugin/AWSCLI.pm and change the function "runScript"
+FlowPlugin::AWSCLI->runStep('Run Script', 'Run Script', 'runScript');
